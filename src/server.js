41bde0d0f79dff1194150aae2b2e65b8672f248b
hagee <hagee.ktr@outlook.com>
2025-03-29 12:58:49 +0300

    Add code that redirects fetch requests to render server to bypass CORS issues

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
-const jsonServer = require('json-server')
-
+/* const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 
@@ -15,4 +14,57 @@ server.use((req, res, next) => {
 })
 server.listen(process.env.PORT || 5000, () => {
     console.log('JSON Server is running')
-})
+}) */
+
+    require("dotenv").config();
+    const jsonServer = require("json-server");
+    const express = require("express");
+    const axios = require("axios");
+    const cors = require("cors");
+    
+    const API_KEY = process.env.add4c8b134724a6d9ebd90930a86980b; 
+    const PORT = process.env.PORT || 5000;
+    
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    
+    const router = jsonServer.router("db.json");
+    const middlewares = jsonServer.defaults();
+    app.use(middlewares);
+    app.use("/api", router);
+    
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "https://vasileiosinnovs.github.io/cryptoczar-project-/"); 
+        res.header("Access-Control-Allow-Headers", "*");
+        next();
+    });
+    
+    app.get("/crypto-news", async (req, res) => {
+        try {
+            const response = await axios.get("https://newsapi.org/v2/everything", {
+                params: {
+                    q: "bitcoin OR ethereum OR dogecoin OR solana OR tether OR cryptocurrency",
+                    pageSize: 10,
+                    apiKey: API_KEY
+                }
+            });
+            res.json(response.data);
+        } catch (error) {
+            res.status(500).json({ error: "Error fetching news" });
+        }
+    });
+
+    app.get("/crypto-data", async (req, res) => {
+        try {
+          const response = await axios.get("https://api.coingecko.com/api/v3/coins/markets", {
+            params: { vs_currency: "usd", order: "market_cap_desc", per_page: 10, page: 1 },
+          });
+          res.json(response.data);
+        } catch (error) {
+          res.status(500).json({ error: "Failed to fetch data" });
+        }
+      });
+      
+    app.listen(PORT, () => console.log(`Proxy running on port ${PORT}`));
+    
